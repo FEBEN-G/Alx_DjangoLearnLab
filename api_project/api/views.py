9408471bfcb47f7c50eca52214d36a0f889c0037
Feben Getachew <febengetachew580@gmail.com>
2025-09-27 16:59:00 +0300

    Implemented DRF API with CRUD and authentication

---
+++ api_project/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+# List API
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Full CRUD ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
