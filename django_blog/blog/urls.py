3d624224a9c168f769161adad2aa3a42c2561c16
Feben Getachew <febengetachew580@gmail.com>
2025-10-02 00:20:51 +0300

    Task 2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,6 @@
-# blog/urls.py
 from django.urls import path
 from . import views
+from . import views 
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -16,6 +16,5 @@ urlpatterns = [
     # Blog Post CRUD URLs
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),  
 ]
